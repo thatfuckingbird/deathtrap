03b1ea355b58ca9c286496518f4d3f10d1681bfe
thatfuckingbird <67429906+thatfuckingbird@users.noreply.github.com>
2021-07-11 20:17:17 +0200

    rename/rewrite: hyfileaccess

--- src/database.cpp
+++ src/database.cpp
@@ -1,6 +1,6 @@
 /*
-deathtrap
-Copyright (C) 2020  Hydroid developers  <hydroid@rbx.run>
+hyfileaccess
+Copyright (C) 2020-2021  Hydroid developers  <hydroid@rbx.run>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
@@ -19,7 +19,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
-#include "database.h"
+#include "hyfileaccess/database.h"
 #include <QSqlDatabase>
 #include <QSqlQuery>
 #include <QDir>
@@ -28,13 +28,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <QSqlError>
 #include <QDebug>
 
-constexpr auto SUPPORTED_VERSIONS = {433};
 constexpr auto DB_NAMES = {"main", "external_caches", "external_mappings", "external_master"};
 constexpr int SQLITE_BUSY_TIMEOUT = 1000 * 60 * 60 * 24 * 7; //A week of timeout should really be enough
-bool currentlyLockedForBackup = false;
-QString dbConnectionUsedForLocking;
 
-bool deathtrap::open(const QString& name, const QString &dbFolderPath, PragmaSynchronous synchronous, PragmaJournalMode journalMode)
+bool hyfileaccess::open(const QString& name, const QString& dbFolderPath, PragmaSynchronous synchronous, PragmaJournalMode journalMode)
 {
     if(!QDir(dbFolderPath).exists()) {
         return false;
@@ -54,69 +51,62 @@ bool deathtrap::open(const QString& name, const QString &dbFolderPath, PragmaSyn
 
     auto db = QSqlDatabase::addDatabase("QSQLITE", name);
     db.setConnectOptions("QSQLITE_BUSY_TIMEOUT=" + QString::number(SQLITE_BUSY_TIMEOUT));
-    db.setDatabaseName(dbFolderPath+"/client.db");
+    db.setDatabaseName(dbFolderPath + "/client.db");
     if(!db.open()) return false;
     QSqlQuery attachQuery{QSqlDatabase::database(name)};
     attachQuery.prepare("attach database ? as external_caches");
     attachQuery.addBindValue(dbFolderPath + "/client.caches.db");
-    if(!attachQuery.exec())
-    {
+    if(!attachQuery.exec()) {
         qDebug() << attachQuery.lastError();
         close(name);
         return false;
     }
     attachQuery.prepare("attach database ? as external_mappings");
     attachQuery.addBindValue(dbFolderPath + "/client.mappings.db");
-    if(!attachQuery.exec())
-    {
+    if(!attachQuery.exec()) {
         qDebug() << attachQuery.lastError();
         close(name);
         return false;
     }
     attachQuery.prepare("attach database ? as external_master");
     attachQuery.addBindValue(dbFolderPath + "/client.master.db");
-    if(!attachQuery.exec())
-    {
+    if(!attachQuery.exec()) {
         qDebug() << attachQuery.lastError();
         close(name);
         return false;
     }
 
-    for(const auto& prefix: DB_NAMES)
-    {
+    for(const auto& prefix: DB_NAMES) {
         QSqlQuery syncQuery{QString{"pragma %1.synchronous = %2"}.arg(prefix, QString::number(static_cast<int>(synchronous))), QSqlDatabase::database(name)};
-        if(!syncQuery.exec())
-        {
+        if(!syncQuery.exec()) {
             qDebug() << syncQuery.lastError();
             close(name);
             return false;
         }
 
         QString journalModeKeyword;
-        switch (journalMode)
-        {
-        case PragmaJournalMode::Delete:
-            journalModeKeyword = "DELETE";
-            break;
-        case PragmaJournalMode::Truncate:
-            journalModeKeyword = "TRUNCATE";
-            break;
-        case PragmaJournalMode::Persist:
-            journalModeKeyword = "PERSIST";
-            break;
-        case PragmaJournalMode::Memory:
-            journalModeKeyword = "MEMORY";
-            break;
-        case PragmaJournalMode::WAL:
-            journalModeKeyword = "WAL";
-            break;
-        case PragmaJournalMode::Off:
-            journalModeKeyword = "OFF";
-            break;
+        switch(journalMode) {
+            case PragmaJournalMode::Delete:
+                journalModeKeyword = "DELETE";
+                break;
+            case PragmaJournalMode::Truncate:
+                journalModeKeyword = "TRUNCATE";
+                break;
+            case PragmaJournalMode::Persist:
+                journalModeKeyword = "PERSIST";
+                break;
+            case PragmaJournalMode::Memory:
+                journalModeKeyword = "MEMORY";
+                break;
+            case PragmaJournalMode::WAL:
+                journalModeKeyword = "WAL";
+                break;
+            case PragmaJournalMode::Off:
+                journalModeKeyword = "OFF";
+                break;
         }
         QSqlQuery journalQuery{QString{"pragma %1.journal_mode = %2"}.arg(prefix, journalModeKeyword), QSqlDatabase::database(name)};
-        if(!journalQuery.exec())
-        {
+        if(!journalQuery.exec()) {
             qDebug() << journalQuery.lastError();
             close(name);
             return false;
@@ -126,13 +116,13 @@ bool deathtrap::open(const QString& name, const QString &dbFolderPath, PragmaSyn
     return true;
 }
 
-void deathtrap::close(const QString& name)
+void hyfileaccess::close(const QString& name)
 {
     QSqlDatabase::database(name).close();
     QSqlDatabase::removeDatabase(name);
 }
 
-int deathtrap::getVersion(const QString& name)
+int hyfileaccess::getVersion(const QString& name)
 {
     QSqlQuery q{"select version from version", QSqlDatabase::database(name)};
     if(!q.exec()) return -1;
@@ -141,12 +131,7 @@ int deathtrap::getVersion(const QString& name)
     return q.value(0).toInt();
 }
 
-QSet<int> deathtrap::supportedVersions()
-{
-    return SUPPORTED_VERSIONS;
-}
-
-std::pair<QStringList, QStringList> deathtrap::getFileLocations(const QString &name)
+std::pair<QStringList, QStringList> hyfileaccess::getFileLocations(const QString& name)
 {
     std::pair<QStringList, QStringList> result;
 
@@ -154,14 +139,12 @@ std::pair<QStringList, QStringList> deathtrap::getFileLocations(const QString &n
     QSqlQuery q{"select location from client_files_locations order by prefix", QSqlDatabase::database(name)};
     if(!q.exec()) return {};
 
-    for(int i = 0; i < 256; i++)
-    {
+    for(int i = 0; i < 256; i++) {
         if(!q.next()) break;
         result.first.append(q.value(0).toString());
     }
 
-    for(int i = 0; i < 256; i++)
-    {
+    for(int i = 0; i < 256; i++) {
         if(!q.next()) break;
         result.second.append(q.value(0).toString());
     }
@@ -171,74 +154,37 @@ std::pair<QStringList, QStringList> deathtrap::getFileLocations(const QString &n
     return result;
 }
 
-bool deathtrap::maintenance::tryLockDatabase(const QString &name)
-{
-    if(currentlyLockedForBackup) return false;
-
-    QSqlQuery q{"begin immediate", QSqlDatabase::database(name)};
-    if(!q.exec())
-    {
-        qDebug() << q.lastError();
-        return false;
-    }
-
-    currentlyLockedForBackup = true;
-    dbConnectionUsedForLocking = name;
-    return true;
-}
-
-bool deathtrap::maintenance::releaseLockedDatabase()
-{
-    if(!currentlyLockedForBackup) return false;
-
-    QSqlQuery q{"end transaction", QSqlDatabase::database(dbConnectionUsedForLocking)};
-    if(!q.exec()) return false;
-
-    currentlyLockedForBackup = false;
-    return true;
-}
-
-bool deathtrap::maintenance::shrinkMemory(const QString &name)
+bool hyfileaccess::maintenance::shrinkMemory(const QString& name)
 {
     QSqlQuery q{"pragma shrink_memory", QSqlDatabase::database(name)};
     return q.exec();
 }
 
-bool deathtrap::maintenance::setMaxAuxiliaryThreadCount(const QString &name, std::uint16_t count)
-{
-    QSqlQuery q{QString{"pragma threads = %1"}.arg(QString::number(count)), QSqlDatabase::database(name)};
-    return q.exec();
-}
-
-QStringList deathtrap::maintenance::integrityCheck(const QString &name, bool quick)
+QStringList hyfileaccess::maintenance::integrityCheck(const QString& name, bool quick)
 {
     QStringList result;
     QString pragmaName = quick ? "quick_check" : "integrity_check";
-    for(const auto& prefix: DB_NAMES)
-    {
+    for(const auto& prefix: DB_NAMES) {
         QSqlQuery checkQuery{QString{"pragma %1.%2"}.arg(prefix, pragmaName), QSqlDatabase::database(name)};
-        if(checkQuery.exec())
-        {
+        if(checkQuery.exec()) {
             while(checkQuery.next()) result.append(checkQuery.value(0).toString());
         }
     }
     return result;
 }
 
-bool deathtrap::maintenance::analyze(const QString &name)
+bool hyfileaccess::maintenance::analyze(const QString& name)
 {
     QSqlQuery q{"analyze", QSqlDatabase::database(name)};
     return q.exec();
 }
 
-bool deathtrap::maintenance::vacuum(const QString &name)
+bool hyfileaccess::maintenance::vacuum(const QString& name)
 {
     bool ok = true;
-    for(const auto& prefix: DB_NAMES)
-    {
+    for(const auto& prefix: DB_NAMES) {
         QSqlQuery vacQuery{QString{"vacuum %1"}.arg(prefix), QSqlDatabase::database(name)};
-        if(!vacQuery.exec())
-        {
+        if(!vacQuery.exec()) {
             qDebug() << vacQuery.lastError();
             ok = false;
         }
@@ -246,21 +192,19 @@ bool deathtrap::maintenance::vacuum(const QString &name)
     return ok;
 }
 
-bool deathtrap::maintenance::optimize(const QString &name)
+bool hyfileaccess::maintenance::optimize(const QString& name)
 {
     QSqlQuery q{"pragma optimize", QSqlDatabase::database(name)};
     return q.exec();
 }
 
-bool deathtrap::maintenance::vacuumInto(const QString &name, const QString &targetDirectory)
+bool hyfileaccess::maintenance::vacuumInto(const QString& name, const QString& targetDirectory)
 {
     bool ok = true;
-    for(const auto& prefix: DB_NAMES)
-    {
+    for(const auto& prefix: DB_NAMES) {
         QSqlQuery vacQuery{QString{"vacuum %1 into ?"}.arg(prefix), QSqlDatabase::database(name)};
         vacQuery.addBindValue(targetDirectory + "/client_" + prefix + ".db");
-        if(!vacQuery.exec())
-        {
+        if(!vacQuery.exec()) {
             qDebug() << vacQuery.lastError();
             ok = false;
         }
--- src/fileserver_main.cpp
+++ src/fileserver_main.cpp
@@ -1,6 +1,6 @@
 /*
-deathtrap
-Copyright (C) 2020  Hydroid developers  <hydroid@rbx.run>
+hyfileaccess
+Copyright (C) 2020-2021  Hydroid developers  <hydroid@rbx.run>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
@@ -20,63 +20,131 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include <QCoreApplication>
-#include "database.h"
-extern "C" { //TODO is this ok
+#include <QCommandLineParser>
+#include <QTextStream>
+#include <QDir>
+#include <QMimeDatabase>
+#include "hyfileaccess/database.h"
+extern "C" {
 #include "3rdparty/mongoose/mongoose.h"
 }
-#include <iostream>
-#include <vector>
 
-//TODO add SSL cert support
-//TODO https://developer.mozilla.org/en-US/docs/Web/HTTP/Range_requests
+static const char* s_debug_level = "2";
 
-static const char *s_debug_level = "2";
-static const char *s_root_dir = ".";
-static const char *s_listening_address = "http://localhost:8017";
+QStringList thumbPaths;
+QStringList filePaths;
+QString listeningAddress;
+QHash<QString, std::pair<QString, QMimeType>> thumbExtensionCache;
+QHash<QString, std::pair<QString, QMimeType>> fileExtensionCache;
 
-std::vector<std::string> tpaths;
-std::vector<std::string> fpaths;
-
-static void cb(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
-  if (ev == MG_EV_HTTP_MSG) {
-      //TODO serve file here
-      auto http_data = static_cast<mg_http_message*>(ev_data);
-      std::cout << std::string{http_data->uri.ptr, http_data->uri.len} << std::endl;
-  }
-  (void) fn_data;
+void updateExtensionCache(const QString& path, QHash<QString, std::pair<QString, QMimeType>>& cache)
+{
+    QDir dir{path};
+    QMimeDatabase mimeDb;
+    for(const auto& entry: dir.entryInfoList(QDir::Files)) {
+        cache[entry.baseName()] = {entry.completeSuffix(), mimeDb.mimeTypeForFile(entry)};
+    }
 }
 
-int main (int argc, char *argv[])
+static std::pair<QString, QString> getPathAndMime(const QString& uri)
 {
-    QCoreApplication app{ argc, argv };
-    if(!deathtrap::open("db", app.arguments().at(1)))
-    {
-        //TODO
+    QString path, hash;
+    std::pair<QString, QMimeType> cachedData;
+    if(uri.startsWith("/thumb/by-hash/")) {
+        hash = uri.mid(15).left(64);
+        if(hash.length() != 64) return {};
+        const int index = hash.leftRef(2).toInt(nullptr, 16);
+        path = thumbPaths[index];
+        cachedData = thumbExtensionCache.value(hash);
+        if(cachedData.first.isEmpty()) {
+            updateExtensionCache(path, thumbExtensionCache);
+        }
+        cachedData = thumbExtensionCache.value(hash);
+    } else if(uri.startsWith("/file/by-hash/")) {
+        hash = uri.mid(16).left(64);
+        if(hash.length() != 64) return {};
+        const int index = hash.leftRef(2).toInt(nullptr, 16);
+        path = filePaths[index];
+        cachedData = fileExtensionCache.value(hash);
+        if(cachedData.first.isEmpty()) {
+            updateExtensionCache(path, fileExtensionCache);
+        }
+        cachedData = thumbExtensionCache.value(hash);
     }
-    const auto [tmpfpaths, tmptpaths] = deathtrap::getFileLocations("db");
-    deathtrap::close("db");
+    if(cachedData.first.isEmpty() || path.isEmpty()) return {};
 
-    for(const auto& p : tmpfpaths) {
-        fpaths.push_back(p.toStdString());
+    return {path + "/" + hash + "." + cachedData.first, cachedData.second.name()};
+}
+
+static void callback(struct mg_connection* c, int ev, void* ev_data, [[maybe_unused]] void* fn_data)
+{
+    if(ev == MG_EV_ACCEPT && mg_url_is_ssl(listeningAddress.toStdString().data())) {
+        struct mg_tls_opts opts = {
+          //.ca = "ca.pem",         // Uncomment to enable two-way SSL
+          .cert = "server.pem", // Certificate PEM file
+          .certkey = "server.pem", // This pem conains both cert and key
+        };
+        mg_tls_init(c, &opts);
+    } else if(ev == MG_EV_HTTP_MSG) {
+        auto http_data = static_cast<mg_http_message*>(ev_data);
+        auto [path, mime] = getPathAndMime(QString::fromUtf8(http_data->uri.ptr, http_data->uri.len));
+        if(path.isEmpty()) {
+            mg_http_reply(c, 404, "", "%s", "Not found\n");
+        } else {
+            mg_http_serve_file(c, http_data, path.toStdString().data(), mime.toStdString().data(), nullptr);
+        }
     }
-    for(const auto& p : tmptpaths) {
-        tpaths.push_back(p.toStdString());
+}
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication app{argc, argv};
+
+    QCommandLineParser cmdLineParser;
+    QCommandLineOption hydrusDbPathOption{QStringList{} << "db-path"
+                                                        << "d",
+                                          "Hydrus database path.", "path"};
+    cmdLineParser.addOption(hydrusDbPathOption);
+    QCommandLineOption listeningAddressOption{QStringList{} << "address"
+                                                            << "a",
+                                              "Listening address.", "address"};
+    cmdLineParser.addOption(listeningAddressOption);
+    cmdLineParser.process(app);
+
+    QTextStream err{stderr};
+
+    if(!hyfileaccess::open("db", cmdLineParser.value(hydrusDbPathOption))) {
+        err << "Failed to open Hydrus database at: " << cmdLineParser.value(hydrusDbPathOption) << Qt::endl;
+        return EXIT_FAILURE;
+    }
+    auto [tmpThumbPaths, tmpFilePaths] = hyfileaccess::getFileLocations("db");
+    thumbPaths = std::move(tmpThumbPaths);
+    filePaths = std::move(tmpFilePaths);
+    hyfileaccess::close("db");
+
+    if(thumbPaths.length() != 256 || thumbPaths.length() != filePaths.length()) {
+        err << "Missing file or thumbnail paths";
+        return EXIT_FAILURE;
     }
 
-    //TODO check length is 256
+    for(const auto& thumbPath: std::as_const(thumbPaths)) {
+        updateExtensionCache(thumbPath, thumbExtensionCache);
+    }
+    for(const auto& filePath: std::as_const(filePaths)) {
+        updateExtensionCache(filePath, fileExtensionCache);
+    }
+
+    listeningAddress = cmdLineParser.value(listeningAddressOption);
 
     mg_mgr mgr;
-    mg_connection *c = nullptr;
     mg_log_set(s_debug_level);
     mg_mgr_init(&mgr);
-    if ((c = mg_http_listen(&mgr, s_listening_address, cb, &mgr)) == NULL)
-    {
-        LOG(LL_ERROR, ("Cannot listen on %s. Use http://ADDR:PORT or :PORT", s_listening_address));
-        exit(EXIT_FAILURE);
+    if(mg_http_listen(&mgr, listeningAddress.toStdString().data(), callback, &mgr) == NULL) {
+        err << "Failed to listen on address: " << listeningAddress << Qt::endl;
+        return EXIT_FAILURE;
     }
 
-    LOG(LL_INFO, ("Starting Mongoose v%s, serving [%s]", MG_VERSION, s_root_dir));
-    for (;;) mg_mgr_poll(&mgr, 1000);
+    for(;;) mg_mgr_poll(&mgr, 1000);
     mg_mgr_free(&mgr);
 
     return EXIT_SUCCESS;
